8bf3b7b823e3026532043ac62574e3d6799ae2b7
2zooo0 <c20221460@cau.ac.kr>
2024-07-02 02:52:03 +0900

    모든 과제 완료

--- week13-prac/src/components/home/FormSection.jsx
+++ week13-prac/src/components/home/FormSection.jsx
@@ -1,13 +1,14 @@
 //form에서 만든걸 사용하는 파일을 만들엇음
 //Form컴포넌트 불러올때 경로 확인 잘하기(import 해온 경로 확인)
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Wrapper } from '../layout/common';
 import Form from './Form';
 import { ThemeColorContext } from '../../context/context';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import { isSubmitedAtom } from '../../recoil/atom';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { emailAtom, isConfirmedAtom, isSubmitedAtom, telAtom, userNameAtom } from '../../recoil/atom';
+import Modal from '../Modal/Modal';
 
 const FormSection = () => {
     const mode = useContext(ThemeColorContext);
@@ -15,11 +16,25 @@ const FormSection = () => {
     const navigate = useNavigate();
 
     //버튼 누르면 제출이 1로 변하게set만필요
+    //const setIsSubmited = useSetRecoilState(isSubmitedAtom);
+
+    //모달관련
+    const userName = useRecoilValue(userNameAtom);
+    const email = useRecoilValue(emailAtom);
+    const isSubmited = useRecoilValue(isSubmitedAtom);
+    const tel = useRecoilValue(telAtom);
     const setIsSubmited = useSetRecoilState(isSubmitedAtom);
 
+    //모달창
+    const [modalOpen, setModalOpen] = useState(false);
+    
+    
+
     const handleBtn = ()=>{
-        navigate("/mypage");
+        //navigate("/mypage"); //모달창으로 기능이동
         setIsSubmited(true);
+        setModalOpen(true);
+      
     }
 
     return (
@@ -28,6 +43,10 @@ const FormSection = () => {
             <Form type='email' inputType={'이메일'} />
             <Form type='tel' inputType={'전화번호'} />
             <Button mode={mode.button} onClick={handleBtn} >제출</Button>
+
+            {modalOpen && (
+                <Modal setModalOpen = {setModalOpen} />
+            )}
         </Wrapper>
     );
 };
